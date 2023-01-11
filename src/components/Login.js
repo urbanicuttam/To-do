96aa4d10c5013d33a76b38b3ab0174e3504bbb5a
Uttam Verma <uttam.verma@tigeranalytics.local>
2023-01-11 21:39:08 +0530

    made changes in login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,18 @@
 import React from 'react';
 import { signInWithGoogle } from '../firebase.js';
 import GoogleButton from 'react-google-button'
+import BackgroundAnimate from './BackgroundAnimate'
 const Login = () => {
       
     return (
         <div>
+            <h1>Hello! {localStorage.getItem("name")}</h1>
             <center>
+                <p>Please Login to make your ToDo!</p>
                 <GoogleButton style={{"marginTop" : "200px"}} 
                 onClick={signInWithGoogle} />
             </center>
+            <BackgroundAnimate />
         </div>
     );
 }
